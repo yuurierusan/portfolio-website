3d7ad3cbf522913ef90fa3e55b7f4050986e9ff0
Jonshea Nutson <yuurieru@icloud.com>
2023-03-09 13:39:10 -0500

    built home component, breaks on resize

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,11 +6,14 @@ const Home = () => {
     return (
         <div
             name='home'
-            className='h-screen w-full bg-gradient-to-b from-black to-gray-800'>
-            <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex=row'>
-                <div>
-                    <h2>I'm a Software Developer</h2>
-                    <p>
+            className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
+            <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+                <div className='flex flex-col justify-center h-full'>
+                    <h2 className='text-4xl sm:text-7xl font-bold text-white'>
+                        I'm a Software Developer
+                    </h2>
+
+                    <p className='text-gray-500 py-4 max-w-md'>
                         I am actively developing my abilities in software
                         development through expert-led instruction in
                         technologies such as HTML, CSS, JS, React, Mongo and
@@ -19,15 +22,20 @@ const Home = () => {
                         technologies. I am also proficient in various computer
                         environments, including Windows, Macintosh, and Linux.
                     </p>
+
                     <div>
-                        <button>
+                        <button className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
                             Projects
-                            <span>
-                                <MdOutlineKeyboardArrowRight />
+                            <span className='group-hover:rotate-90 duration-300'>
+                                <MdOutlineKeyboardArrowRight
+                                    size={25}
+                                    className='ml-1'
+                                />
                             </span>
                         </button>
                     </div>
                 </div>
+
                 <img
                     src={HeroImage}
                     alt='my profile'
