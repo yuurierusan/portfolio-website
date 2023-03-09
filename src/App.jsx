0dac9d255c439daf1faed190409b7c7e118111c3
Jonshea Nutson <yuurieru@icloud.com>
2023-03-09 12:17:15 -0500

    added name to nav component and import to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import NavBar from './components/NavBar'
+
 const App = () => {
-    return <div></div>
+    return (
+        <div>
+            <NavBar />
+        </div>
+    )
 }
 
 export default App
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,13 @@
+import React from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
+
 const NavBar = () => {
-    return <div>NavBar</div>
+    return (
+        <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed'>
+            <div>
+                <h1 className='text-5xl font-signature ml-2'>Jonshea</h1>
+            </div>
+        </div>
+    )
 }
 export default NavBar
