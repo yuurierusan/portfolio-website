f314187f232242ed153cf3bd039dbb273428af00
Jonshea Nutson <yuurieru@icloud.com>
2023-04-19 13:13:47 -0400

    updated projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,35 +1,34 @@
-import arrayDestruct from '../assets/portfolio/arrayDestruct.jpg'
-import installNode from '../assets/portfolio/installNode.jpg'
-import navbar from '../assets/portfolio/navbar.jpg'
-import reactParallax from '../assets/portfolio/reactParallax.jpg'
-import reactSmooth from '../assets/portfolio/reactSmooth.jpg'
-import reactWeather from '../assets/portfolio/reactWeather.jpg'
-
 const Projects = () => {
     const projects = [
         {
             id: 1,
-            src: arrayDestruct,
+            src: 'https://raw.githubusercontent.com/mannyaalonso/stockwise/main/public/ss6.png',
+            href: 'https://github.com/mannyaalonso/stockwise',
+            href2: 'https://stockwise-fe.netlify.app/',
         },
         {
             id: 2,
-            src: reactParallax,
+            src: 'https://static.vecteezy.com/system/resources/previews/003/582/701/original/coming-soon-background-illustration-template-design-free-vector.jpg',
+            href: 'https://github.com/yuurierusan/Habitat-Hunter',
+            href2: 'https://static.vecteezy.com/system/resources/previews/003/582/701/original/coming-soon-background-illustration-template-design-free-vector.jpg',
         },
         {
             id: 3,
-            src: navbar,
+            src: 'https://raw.githubusercontent.com/yuurierusan/Whiteboard/main/HomePage.jpeg',
+            href: 'https://github.com/yuurierusan/Whiteboard',
+            href2: 'https://static.vecteezy.com/system/resources/previews/003/582/701/original/coming-soon-background-illustration-template-design-free-vector.jpg',
         },
         {
             id: 4,
-            src: reactSmooth,
+            src: 'https://raw.githubusercontent.com/yuurierusan/Top-G-Auto-App/main/client/public/ss2.png',
+            href: 'https://github.com/yuurierusan/Top-G-Auto-App',
+            href2: 'https://top-g-auto.herokuapp.com/',
         },
         {
             id: 5,
-            src: installNode,
-        },
-        {
-            id: 6,
-            src: reactWeather,
+            src: 'https://raw.githubusercontent.com/yuurierusan/Mine_Sweeper/main/gamepage.png',
+            href: 'https://github.com/yuurierusan/Mine_Sweeper',
+            href2: 'https://mine_sweeper.surge.sh/',
         },
     ]
 
@@ -46,7 +45,7 @@ const Projects = () => {
                 </div>
 
                 <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
-                    {projects.map(({ id, src }) => (
+                    {projects.map(({ id, src, href, href2 }) => (
                         <div
                             key={id}
                             className='shadow-md shadow-gray-600 rounded-lg'>
@@ -55,12 +54,16 @@ const Projects = () => {
                                 alt=''
                                 className='rounded-md duration-200 hover:scale-105'
                             />
-                            <div className='flex items-center justify-center'>
+                            <div
+                                onClick={href}
+                                className='flex items-center justify-center'>
                                 <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>
-                                    Demo
+                                    GitHub
                                 </button>
-                                <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>
-                                    Code
+                                <button
+                                    onClick={href2}
+                                    className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>
+                                    Deployed
                                 </button>
                             </div>
                         </div>
